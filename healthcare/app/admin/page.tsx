108a5810720f1240b923ea27d31062bdf3b4749d
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2025-02-19 22:17:08 +0530

    Update files

--- healthcare/app/admin/page.tsx
+++ healthcare/app/admin/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 
 import { StatCard } from "@/components/StatCard";
--- healthcare/components/forms/AppointmentForm.tsx
+++ healthcare/components/forms/AppointmentForm.tsx
@@ -101,6 +101,7 @@ export const AppointmentForm = ({
             cancellationReason: values.cancellationReason,
           },
           type,
+          timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone, // ✅ Fix: Add timeZone
         };
 
         const updatedAppointment = await updateAppointment(appointmentToUpdate);
